46526d5f251486e98080731088c88f67bd5df4af
Ilya Kisil <ilyakisil@gmail.com>
2018-12-04 17:13:56 +0000

    Standardised start of jupyter notebook. Now it is in the same was as JL but with addition option

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -10,14 +10,14 @@ _FILE_NAME=`basename $0`
 cat << HELP_USAGE
 
 Description:
-    This script starts a Jupyter Lab.
+    This script starts a Jupyter Lab or Jupyter Notebook.
 
-Usage: $_FILE_NAME [-h|--help] [-p=|--port=<port>]
+Usage: $_FILE_NAME [-h|--help] [-p=|--port=<port>] [-n|--notebook]
 
 Examples:
 	$_FILE_NAME
 	$_FILE_NAME -p=9000
-	$_FILE_NAME --port=9000
+	$_FILE_NAME -n --port=9000
 
 Options:
     -h|--help
@@ -27,8 +27,13 @@ Options:
     	Use port for starting/connecting to.
     	It is advised to use value '>=10000'.
 
-    	Defaults to the value of \$JUPYTER_LAB_PORT
-    	or '8888' if this env variable doesn't exist.
+    	Defaults to the values:
+        1) \$JUPYTER_LAB_PORT for Jupyter Lab
+        2) \$JUPYTER_NOTEBOOK_PORT for Jupyter Notebook
+    	3) '8888' if these env variables do not exist
+
+    -n|--notebook
+        Start Jupyter Notebook instead of Jupyter Lab.
 
 Author:
     Ilya Kisil <ilyakisil@gmail.com>
@@ -45,6 +50,7 @@ if [ ! -z $JUPYTER_LAB_PORT ];then
 else
     port="8888"
 fi
+start_type=0
 
 # Parse arguments
 for arg in "$@"; do
@@ -56,6 +62,9 @@ for arg in "$@"; do
         -p=*|--port=*)
             port="${arg#*=}"
             ;;
+        -n|--notebook)
+            start_type=1
+            ;;
         *)
             # Skip unknown option
             ;;
@@ -64,6 +73,24 @@ for arg in "$@"; do
 done
 
 
-printf "Starting Jupyter Lab for remote use\n"
-# Locally define $SHELL, so that terminals opened in JupyterLab would use zsh
-env SHELL="`which zsh`" jupyter lab --no-browser --port="${port}"
+if [[ ($start_type == 0) ]]; then
+
+	printf "Starting Jupyter Lab for remote use\n"
+
+    if [ ! -z $JUPYTER_LAB_PORT ];then
+        port="${JUPYTER_LAB_PORT}"
+    fi
+
+	# Locally define $SHELL, so that terminals opened in JupyterLab would use zsh
+    env SHELL="`which zsh`" jupyter lab --no-browser --port="${port}"
+
+elif [[ ($start_type == 1) ]]; then
+
+	printf "Starting Jupyter Notebook for remote use\n"
+
+	if [ ! -z $JUPYTER_NOTEBOOK_PORT ];then
+        port="${JUPYTER_NOTEBOOK_PORT}"
+    fi
+
+	jupyter notebook --no-browser --port=${port}
+fi
